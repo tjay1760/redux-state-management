2eb29db08bb7e6f26fe117b9ebbbd5171443e041
John Murianki <mcjthiongo@gmail.com>
2023-08-09 17:50:16 +0300

    Add thunk

--- redux-toolkit/user/userSlice.js
+++ redux-toolkit/user/userSlice.js
@@ -1,10 +1,14 @@
 const createSlice = require("@reduxjs/toolkit").createSlice;
 const createAsyncThunk = require("@reduxjs/toolkit").createAsyncThunk; 
+const axios = require('axios')
 const initialState ={
     loading:false,
     users:[],
     error:''
     };
+    const fetchUsers = createAsyncThunk("use/fetchUsers"()=>{
+        
+    })
 const UserSlice = createSlice({
     name: "user",
 
